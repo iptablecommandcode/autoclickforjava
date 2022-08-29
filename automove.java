5b7d32d491075e03bef9fbf6065e073417633a0b
박치원 <iptablecommandcode@gmail.com>
2022-08-29 14:17:54 +0900

    FINISH V0.1

--- automove.java
+++ automove.java
@@ -6,19 +6,22 @@ import java.util.Calendar;
 //import java.awt.MouseInfo;
 
 public class automove {
-    final static int MILLITOSECCALC = 60000;// 60*1000
+    final static int MILLITOSECCALC = 2 * 60 * 1000;// 60*1000
 
     public static void main(String[] args) throws AWTException, InterruptedException {
-
+        long timecheck;
         while (true) {
             automove auto = new automove();
             // weekend didn't work program
             if (auto.CalendarCheckWeekend()) {
-            }else{
-                // button sta
-                automove.shiftAction();
-                // program pause
-                Thread.sleep(auto.CalendarCheckTime());
+            } else {
+                timecheck = auto.CalendarCheckTime();
+                if (timecheck != 0) {
+                    // button sta
+                    automove.shiftAction();
+                    // program pause
+                    Thread.sleep(timecheck);
+                }
             }
         }
     }
@@ -26,17 +29,17 @@ public class automove {
     // keyboard keypress, keyrelease (ctrl)
     private static void shiftAction() throws AWTException {
         Robot robot = new Robot();
-        //PointerInfo info;
-        //key press
+        // PointerInfo info;
+        // key press
         robot.keyPress(17);
         robot.keyRelease(17);
 
         // mouse move
-        //info = MouseInfo.getPointerInfo();
-        //robot.mouseMove(info.getLocation().x+1, info.getLocation().y+1);
+        // info = MouseInfo.getPointerInfo();
+        // robot.mouseMove(info.getLocation().x+1, info.getLocation().y+1);
     }
 
-    //weekend check
+    // weekend check
     private boolean CalendarCheckWeekend() {
         Calendar cal = Calendar.getInstance();
         if ((cal.get(Calendar.DAY_OF_WEEK) == 1) || (cal.get(Calendar.DAY_OF_WEEK) == 7)) {
@@ -45,39 +48,32 @@ public class automove {
         return false;
     }
 
-    //stop time move for time
+    // stop time move for time
     private long CalendarCheckTime() {
         Calendar cal = Calendar.getInstance();
-        int hour, minute;
-        hour = cal.get(Calendar.HOUR);
-        minute = cal.get(Calendar.MINUTE);
+        int hour, AM_PM;
 
-        // 09 O'clock start
-        if (hour >= 9) {
-            // 12 O'clock launch time before 13 O'clock
-            if (hour >= 12) {
-                if (hour < 13) {
-                    minute = 60 - minute;
-                    return minute * MILLITOSECCALC;// 13 O'clock - timenow
-                } else if (hour < 18) {// before 18 O'clock 2 minute
-                    return 2 * MILLITOSECCALC;
-                } else {
-                    // after 18 O'clock 09 O'clock
-                    hour = 24 - hour + 9;
-                    return hour * 60 * MILLITOSECCALC;
-                }
-            } else if (hour < 18) {
-                // before 18 O'clock 2minute
-                return 2 * MILLITOSECCALC;
+        AM_PM = cal.get(Calendar.AM_PM);
+        hour = cal.get(Calendar.HOUR);
+
+        if (AM_PM == 0) {// AM
+            if (hour >= 9) {
+                return MILLITOSECCALC;
             } else {
-                // after 18 O'clock 09 O'clock
-                hour = 24 - hour + 9;
-                return hour * 60 * MILLITOSECCALC;
+                return 0;
             }
-        } else {
-            // after 00 O'clock start time
-            hour = 9 - hour;
-            return hour * 60 * MILLITOSECCALC;
+        } else {// PM
+            if (hour == 0) {
+                return 0;
+            } else if (hour >= 1) {
+                if (hour >= 6) {
+                    return 0;
+                } else {
+                    return MILLITOSECCALC;
+                }
+            } else {
+                return 0;
+            }
         }
     }
 }
